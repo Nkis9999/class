b255fcd063bec628f2ccb3c9da43944a15a410c8
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2025-12-20 12:36:11 +0800

    create UsersRepostitory & connoct MySQL Data

---
+++ exp-login-lab/src/main/java/com/course/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.course.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.course.entity.UsersEntity;
+
+@Repository
+public interface UsersRepository extends JpaRepository<UsersEntity, Integer>{
+	
+}
--- exp-login-lab/src/main/java/com/course/service/LoginService.java
+++ exp-login-lab/src/main/java/com/course/service/LoginService.java
@@ -1,14 +1,24 @@
 package com.course.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.course.entity.UsersEntity;
 import com.course.model.UserVo;
+import com.course.repository.UsersRepository;
 
 // @Component
 @Service
 public class LoginService {
+	
+	@Autowired
+	private UsersRepository usersRepository;
 
 	public boolean checkLogin(String username, String password) {
+		List<UsersEntity> users = usersRepository.findAll();
+		
 		if (username.equals("aaa") && password.equals("111")) {
 			return true;
 		} else {
