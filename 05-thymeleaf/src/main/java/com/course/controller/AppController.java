206d87cf2ff0dc6be6ca7c8a893c663a5cf66fbe
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2025-12-27 16:34:38 +0800

    回家繼續完成

--- 05-thymeleaf/src/main/java/com/course/controller/AppController.java
+++ 05-thymeleaf/src/main/java/com/course/controller/AppController.java
@@ -1,19 +1,44 @@
 package com.course.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.course.model.UserVo;
+
 @Controller
 public class AppController {
+
+	private static final List<UserVo> userList = new ArrayList<>();
 	
+    static {
+    	userList.add(new UserVo(1, "芙莉蓮", "111", "aaa@aaa.com"));
+    	userList.add(new UserVo(2, "賽倫", "222", "bbb@bbb.com"));
+    	userList.add(new UserVo(3, "星野愛", "333", "ccc@ccc.com"));
+    }
+    
 	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("title","首頁");
-		model.addAttribute("param1","minecraft");
-		model.addAttribute("param2","hero");
-//		model.addAttribute("title","<span style=\"color:DodgerBlue;\">去唱卡拉ＯＫ吧</span>");
-//		model.addAttribute("title2", "<script>alert('你被攻擊了')</script>");
+		model.addAttribute("title", "<span style=\"color:DodgerBlue;\">去唱卡拉ＯＫ吧</span>");
+		
+		model.addAttribute("title2", "<script>alert('你被攻擊了')</script>");
+		
+		model.addAttribute("param1", "半邊天");
+		
+		model.addAttribute("param2", "福利蓮");
+		
+		model.addAttribute("users", userList);
 		return "app";
+	}
+	
+	public String test() {
+		String t = """
+				
+				""";
+		
+		return t;
 	}
 }
---
+++ 05-thymeleaf/src/main/java/com/course/model/UserVo.java
@@ -0,0 +1,21 @@
+package com.course.model;
+
+public class UserVo {
+	 private Integer id;
+	    
+	    private String username;
+	    
+	    private String password;
+	    
+	    private String email;
+
+		public UserVo(Integer id, String username, String password, String email) {
+			super();
+			this.id = id;
+			this.username = username;
+			this.password = password;
+			this.email = email;
+		}
+	    
+	    
+}
