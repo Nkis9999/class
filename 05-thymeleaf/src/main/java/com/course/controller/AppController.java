06bc6298bc707b0d4df86a905c94f59e9bcfed85
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2025-12-27 16:08:44 +0800

    utex & 字串模板

--- 05-thymeleaf/src/main/java/com/course/controller/AppController.java
+++ 05-thymeleaf/src/main/java/com/course/controller/AppController.java
@@ -10,6 +10,10 @@ public class AppController {
 	@GetMapping("/home")
 	public String home(Model model) {
 		model.addAttribute("title","首頁");
+		model.addAttribute("param1","minecraft");
+		model.addAttribute("param2","hero");
+//		model.addAttribute("title","<span style=\"color:DodgerBlue;\">去唱卡拉ＯＫ吧</span>");
+//		model.addAttribute("title2", "<script>alert('你被攻擊了')</script>");
 		return "app";
 	}
 }
