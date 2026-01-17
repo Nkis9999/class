eb80b9ccb8a4af7fe0c5fcfb008c1396fc6bd556
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2026-01-17 09:50:14 +0800

    logout

--- exp-book-lab/src/main/java/com/course/service/UserService.java
+++ exp-book-lab/src/main/java/com/course/service/UserService.java
@@ -5,19 +5,31 @@ import org.springframework.stereotype.Service;
 
 import com.course.model.UserSession;
 
+import jakarta.servlet.http.HttpSession;
+
 @Service
 public class UserService {
 
 	@Autowired
 	private UserSession userSession;
-	
+
+	@Autowired
+	private HttpSession httpSession;
+
 	public boolean login(String username, String password) {
 		// 紀錄 Session 資料
 		userSession.setUsername(username);
 		return true;
 	}
-	
+
 	public String getUsernameFromSession() {
 		return userSession.getUsername();
+	}
+
+	public void logout() {
+		// userSession.setUsername("");
+		// userSession.setUsername(null);
+		// 把 Session 清除
+		httpSession.invalidate();
 	}
 }
