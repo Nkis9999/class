d690935235d2a1549bddc5ca9ed6ffe4c573c5d3
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2026-01-17 11:37:01 +0800

    CORS

---
+++ 06-restful/src/main/java/com/course/config/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.course.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+//@Configuration	
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-type");
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        System.out.println("********************************* CORS Filter **************************");
+        chain.doFilter(req, res);
+    }
+}
--- 06-restful/src/main/java/com/course/controller/RestfulController.java
+++ 06-restful/src/main/java/com/course/controller/RestfulController.java
@@ -26,9 +26,9 @@ import io.swagger.v3.oas.annotations.Operation;
 //@ResponseBody
 @RestController
 //@CrossOrigin("http://127.0.0.1:5500/")
-//@CrossOrigin("*")
+@CrossOrigin("*")
 //@CrossOrigin(value = {"http://127.0.0.1:5500", "http://localhost:3000"})
-@CrossOrigin(origins = { "http://127.0.0.1:5500" }, allowedHeaders = "*", allowCredentials = "true")
+//@CrossOrigin(origins = { "http://127.0.0.1:5500" }, allowedHeaders = "*", allowCredentials = "true")
 public class RestfulController {
 
 	@Operation(tags = {"測試TAG", "測試TAG2"}, summary= "你好", description="我是描述")
